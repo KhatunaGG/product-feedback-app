f12122357a5895ab1cc65aa02796883a107523a0
Gagloshvili <KGagloshvili@oriflame.com>
2025-10-27 17:36:56 +0400

    added: header-markup

--- src/app/components/__atoms/index.ts
+++ src/app/components/__atoms/index.ts
@@ -1,1 +1,4 @@
 export { default as Close } from "./close/Close";
+export { default as Bulb } from "./bulb/Bulb";
+export { default as ChevronDown } from "./chevronDown/ChevronDown";
+export { default as Check } from "./check/Check";
--- src/app/components/__organism/index.ts
+++ src/app/components/__organism/index.ts
@@ -2,3 +2,4 @@ export { default as Dashboard } from "./dashboard/Dashboard";
 export { default as Nav } from "./nav/Nav";
 export { default as NavOptions } from "./navOptions/NavOptions";
 export { default as NavRoadMap } from "./navRoadMap/NavRoadMap";
+export { default as Header } from "./header/Header";
